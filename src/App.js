2556bdef7eea02aa335786ea7af056404e33ae48
Михаил Харлов <p4kc0dn43eml1@gmail.com>
2022-01-17 19:12:27 +0300

    redux main features practice

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+
+import React from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <React.Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </React.Fragment>
+  );
+}
+
+export default App;
